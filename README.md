5c58e37abddb1e2d7748ad41e1811728e0e4ad54
Ferdinand Hübner <ferdinand.huebner@gmail.com>
2015-10-03 14:40:11 +0200

    export all episodes to csv and json; read username/password from stdin

---
+++ README.md
@@ -0,0 +1,9 @@
+# myepisodes-export
+
+Exports your all your shows and episodes from http://www.myepisodes.com to a CSV and JSON file.
+
+## Usage
+
+* install node.js
+* run `npm install`
+* run `node index.js`
--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 "use strict";
 
-var request = require('request');
-var deferred = require('deferred');
+var request = require("request");
+var deferred = require("deferred");
 var jsdom = require("jsdom");
+var fs = require("fs");
+var _ = require("underscore");
+var prompt = require('prompt');
 
 var cookieJar = request.jar();
 
@@ -41,12 +44,10 @@ var getShows = function() {
       var $ = window.$;
       $("#shows").find("option").each(function (idx, elem) {
         var option = $(this);
-        if (shows.length == 0) {
-          shows.push({
-            id: option.attr("value"),
-            name: option.text()
-          });
-        }
+        shows.push({
+          id: option.attr("value"),
+          name: option.text()
+        });
       });
       d.resolve(shows);
     }
@@ -64,42 +65,101 @@ var processShow = function(show) {
     scripts: ["http://code.jquery.com/jquery.js"],
     jar: cookieJar,
     done: function (errors, window) {
+      var episodes = [];
       var $ = window.$;
+
+      var tvrageId = "";
+      try {
+        tvrageId = $("a[href^='http://www.tvrage.com/shows/id-']").attr("href").split("-")[1];
+      } catch (err) {
+        //
+      }
+
       $("tr[class^='Episode_']").each(function(idx, elem) {
         var episode = {};
+        episode.tvrageId = tvrageId;
         $(elem).find("td").each(function (idx, elem) {
           var td = $(elem);
           if (td.hasClass("showname")) {
             episode.showname = td.text();
           } else if (td.hasClass("longnumber")) {
+            var x = td.text().split("x");
+            try {
+              episode.season = parseInt(x[0]);
+            } catch (err) {
+
+            }
+            try {
+              episode.episode = parseInt(x[1]);
+            } catch (err) {
+
+            }
             episode.longnumber = td.text();
           } else if (td.hasClass("status")) {
             var input = $(td.children(":first"));
             if (input.attr("onclick").indexOf("Acquired") != -1) {
-              episode.acquired = input.is(':checked');
+              episode.acquired = input.is(":checked");
             } else if (input.attr("onclick").indexOf("Viewed") != -1) {
-              episode.viewed = !!input.is(':checked');
+              episode.viewed = input.is(":checked");
             }
           }
         });
-        console.log(episode);
+        episodes.push(episode);
       });
-      d.resolve();
+      d.resolve(episodes);
     }
   });
 
   return d.promise();
 };
 
+var writeEpisodes = function(episodes) {
+  var now = function() {
+    function pad(number) {
+      if (number < 10) {
+        return '0' + number;
+      }
+      return number;
+    }
+    var d = new Date();
+    return d.getFullYear() + "-" + pad(d.getMonth() + 1) + "-"
+      + pad(d.getDate()) + "_" + pad(d.getHours()) + pad(d.getMinutes()) + pad(d.getSeconds());
+  };
+  var d = now();
 
-
-login("USER", "PASS").then(function () {
-  console.log("Login successful.");
-
-  getShows().map(deferred.gate(function(show) {
-    return processShow(show);
-  }, 1)).then(function(){
-    console.log("All done!");
+  var csvData = "show name;tvrage id;season;episode;long episode;acquired;viewed\n" + episodes.map(function (episode) {
+    return episode.showname + ";" + episode.tvrageId + ";" + episode.season + ";" + episode.episode + ";"
+      + episode.longnumber + ";" + episode.acquired + ";" + episode.viewed + "\n"
+  }).reduce(function (acc, episode) {
+    return acc + episode;
   });
 
+  fs.writeFileSync("myepisodes-export-" + d + ".csv", csvData);
+  fs.writeFileSync("myepisodes-export-" + d + ".json", JSON.stringify(episodes));
+};
+
+prompt.message = "";
+prompt.delimiter = "";
+
+console.log("Please enter username and password for http://www.myepisodes.com");
+
+prompt.start();
+
+
+prompt.get(["username", "password"], function (err, result) {
+  if (err) {
+    console.log("Error reading username and password");
+  } else {
+    login(result.username, result.password).then(function () {
+      console.log("Login successful.");
+
+      getShows().map(deferred.gate(function(show) {
+        return processShow(show);
+      }, 1)).then(function(episodes){
+        writeEpisodes(_.flatten(episodes));
+        console.log("\nAll done. " + _.flatten(episodes).length + " episodes exported");
+      });
+
+    });
+  }
 });
